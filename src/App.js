585a5ffb0cb683787eaf5da938995bde9749ce8e
Rengarajan <renga.anbil@gmail.com>
2024-12-02 15:19:43 +0530

    fix: added space
    test to take lighthouse audit in github pipelines

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, <code>world!</code>.
+          Hello,<code> world!</code>.
         </p>
         <a
           className="App-link"
